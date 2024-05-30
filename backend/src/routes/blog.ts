613c5c5772b3736ce5273838392402cf5d8a0a1e
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-05-31 00:46:39 +0530

    Added front-end part

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -101,7 +101,18 @@ blogRouter.get('/bulk', async (c) => {
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
 
-    const blogs = await prisma.post.findMany()
+    const blogs = await prisma.post.findMany({
+        select: {
+            title: true,
+            id: true,
+            content: true,
+            author: {
+                select: {
+                    name: true,
+                }
+            }
+        }
+    })
     
     return c.json({
         blogs,
@@ -118,6 +129,16 @@ blogRouter.get('/:id', async (c) => {
         const blog = await prisma.post.findFirst({
             where: {
                 id: id,
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        name: true,
+                    }
+                }
             }
         })
         return c.json({
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.ansarishahbaz96.workers.dev";
